06807d0cccb41a55a8ec160ee9016b336df0fe46
Ernest Dodson <ernestdodson@mac.lan>
2026-04-06 20:38:18 -0500

    chore: add PM2 ecosystem config with PORT=3002

---
+++ ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  apps: [{
+    name: 'savingkc-crm',
+    script: 'npm',
+    args: 'start',
+    cwd: '/Users/ernestdodson/savingkc-crm',
+    env: {
+      PORT: 3002,
+      NODE_ENV: 'production',
+    },
+  }],
+}
