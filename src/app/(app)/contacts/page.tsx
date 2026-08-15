a6f318b2e2f42dff5f5762e5718f265158d26a2a
Ernest Dodson <ernest@savingkc.com>
2026-08-14 22:21:34 -0500

    remove obsolete lint suppression

--- src/app/(app)/contacts/page.tsx
+++ src/app/(app)/contacts/page.tsx
@@ -1,5 +1,4 @@
 'use client'
-/* eslint-disable react-hooks/set-state-in-effect, react-hooks/purity -- Existing workspace state hydration and clock-based filters are intentionally client-driven. */
 
 import Link from 'next/link'
 import { useRouter } from 'next/navigation'
